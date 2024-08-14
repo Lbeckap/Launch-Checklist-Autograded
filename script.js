420a712f1bf20812b7769dfcb0a6b355be5e5abb
Lori <148814155+Lbeckap@users.noreply.github.com>
2024-08-14 11:20:23 -0400

    A5 all tests pass

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 // Write your JavaScript code here!
-
-
-//TODO: this is causing the error: Uncaught SyntaxError: redeclaration of var validateInput
-//TODO: Why aren't my console logs for validation Input showing?
-const { validateInput } = require("./scriptHelper");
-
 window.addEventListener("load", function() {
-
+    const pilot = document.getElementById('pilotName').value.trim();
+    const copilot = document.getElementById('coPilotName').value.trim();
+    const fuelLevel = document.getElementById('fuelLevel').value.trim();
+    const cargoLevel = document.getElementById('cargoMass').value.trim();
+    const list = document.getElementById('faultyItems');
     const form = document.getElementById('testForm');
-    form.addEventListener('submit', validateInput); //TODO: no parenthesis with the call back function right?
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
+    });
 
     let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse;
+    let listedPlanetsResponse = myFetch();
     listedPlanetsResponse.then(function (result) {
         listedPlanets = result;
         console.log(listedPlanets);
+
     }).then(function () {
-        console.log(listedPlanets);
         // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-    })
-    
+        const planet = pickPlanet(listedPlanets);
+        addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image);
+    }) 
  });
--- scriptHelper.js
+++ scriptHelper.js
@@ -3,53 +3,91 @@
 require('cross-fetch/polyfill');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-    // Here is the HTML formatting for our mission target div.
-    /*
-                 <h2>Mission Destination</h2>
-                 <ol>
-                     <li>Name: </li>
-                     <li>Diameter: </li>
-                     <li>Star: ${star}</li>
-                     <li>Distance from Earth: </li>
-                     <li>Number of Moons: </li>
-                 </ol>
-                 <img src="">
-    */
- }
- 
- function validateInput(testInput) {
-    const pilotName = document.getElementById('pilotName');
-    const copilotName = document.getElementById('copilotName');
-    const fuelLevel = document.getElementById('fuelLevel');
-    const cargoMass = document.getElementById('cargoMass');
+    const missionTarget = document.getElementById('missionTarget');
+    missionTarget.innerHTML = `
+    <h2>Mission Destination</h2>
+     <ol>
+        <li>Name:${name}</li>
+        <li>Diameter:${diameter}</li>
+        <li>Star:${star}</li>
+        <li>Distance from Earth:${distance}</li>
+        <li>Number of Moons:${moons}</li>
+    </ol>
+    <img src="${imageUrl}">
+    `;
+}
 
-    console.log(pilotName.value);
-    console.log(copilotName.value);
-    console.log(fuelLevel.value);
-    console.log(cargoMass.value);
+function validateInput(testInput) {
+    if (testInput === "") {
+        return 'Empty';
+    } else if (isNaN(testInput)) {
+        return 'Not a Number';
+    } else if (!isNaN(testInput)) {
+        return 'Is a Number';
+    }
+}
 
+function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+    if (validateInput(pilot) !== "Not a Number") {
+        alert('Pilot is required and must contain letters only.');
+    }
+    if (validateInput(copilot) !== "Not a Number") {
+        alert('CoPilot is is required and must contain letters only.');
+    }
+    if (validateInput(fuelLevel) !== "Is a Number") {
+        alert('Fuel Level must be a number.');
+    }
+    if (validateInput(cargoLevel) !== "Is a Number") {
+        alert('Cargo Level must be a number.');
+    }
 
-    event.defaultPrevented();
- }
- 
- function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-    
- }
- 
- async function myFetch() {
-     let planetsReturned;
- 
-     planetsReturned = await fetch().then( function(response) {
-         });
- 
-     return planetsReturned;
- }
- 
- function pickPlanet(planets) {
- }
- 
- module.exports.addDestinationInfo = addDestinationInfo;
- module.exports.validateInput = validateInput;
- module.exports.formSubmission = formSubmission;
- module.exports.pickPlanet = pickPlanet; 
- module.exports.myFetch = myFetch;
+    const launchStatus = document.getElementById('launchStatus');
+    launchStatus.innerHTML = `Shuttle is Ready for Launch`;
+    launchStatus.style.color = 'green';
+
+    function notReady() {
+        launchStatus.innerHTML = `Shuttle Not Ready for Launch`;
+        launchStatus.style.color = 'red';
+    }
+
+    const fuelStatus = document.getElementById('fuelStatus');
+    const cargoStatus = document.getElementById('cargoStatus');
+    if (fuelLevel < 10000) {
+        fuelStatus.innerHTML = `Fuel level too low for launch`;
+        notReady();
+    } else {
+        fuelStatus.innerHTML = `Fuel level high enough for launch`;
+    }
+    if (cargoLevel > 10000) {
+        cargoStatus.innerHTML = `Cargo mass too heavy for launch`;
+        notReady();
+    } else {
+        cargoStatus.innerHTML = `Cargo mass low enough for launch`;
+    }
+
+    list.style.visibility = 'visible';
+
+    const pilotReady = document.getElementById('pilotStatus');
+    pilotReady.innerHTML = `Pilot ${pilot} is ready for launch`;
+
+    const coPilotStatus = document.getElementById('copilotStatus');
+    coPilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
+}
+
+async function myFetch() {
+    let planetsReturned;
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json");
+    let data = await planetsReturned.json();
+    return data;
+}
+
+function pickPlanet(planets) {
+    let index = Math.floor(Math.random() * planets.length);
+    return planets[index];
+}
+
+module.exports.addDestinationInfo = addDestinationInfo;
+module.exports.validateInput = validateInput;
+module.exports.formSubmission = formSubmission;
+module.exports.pickPlanet = pickPlanet;
+module.exports.myFetch = myFetch;
